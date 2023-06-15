5f12e68f95cc2046265bfff03baabb74cadf0b44
amirkeyire <amir.keyire@gmail.com>
2023-06-15 14:22:55 -0700

    amir

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - A program that prints a line with puts function
  *
- * Return: 0 on (success)
+* Return: 0 on success. 
  */
 int main(void) {
   puts("\"Programming is like building a multilingual puzzle");
