9268a9a5510bcc0d26759fa69143a11c6325c72c
amirkeyire <amir.keyire@gmail.com>
2023-06-15 14:19:27 -0700

    amir

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - A program that prints a line with puts function
  *
- * Return: 0 on success.
+ * Return: 0 on (success)
  */
 int main(void) {
-  puts("Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }
