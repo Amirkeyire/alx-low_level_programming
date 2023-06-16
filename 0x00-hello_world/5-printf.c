84c1e91a9830cdf0fcdc099b000aa6215c474278
amirkeyire <amir.keyire@gmail.com>
2023-06-16 01:22:46 -0700

    amir

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include (studio.h)
+/**
+* main - A program taht print a line using the printf f
+unction
+* Return: 0 (Success)
+*/
+
+int main (void)
+{
+         printf("with proper gramar, but the outcome is
+a place of art,\n")
+          return (0);
+}
